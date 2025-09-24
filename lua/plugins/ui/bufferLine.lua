e06aec423dfb86c21f9a599cb768cecc2a9767b8
harry feldman <harry.feldman99@gmail.com>
2025-09-24 08:21:02 -0500

    added move-tab keybindings

--- lua/plugins/ui/bufferLine.lua
+++ lua/plugins/ui/bufferLine.lua
@@ -4,9 +4,15 @@ local util = require('util')
 local icons = require('icons')
 
 return {
-  "akinsho/bufferline.nvim",
-  event = "VeryLazy",
+  "hjfeldy/bufferline.nvim",
+  -- dir = "/home/harry/Repos/bufferline.nvim/",
+  branch = 'feature/resession',
+  lazy = false,
+  -- event = "VeryLazy",
   keys = util.mergeArrays({
+
+    { "gi", "gi", noremap=true }, -- not sure why this is necessary, but the builtin gi binding is getting remapped to a no-op at somewhere
+
     { "g", "", desc = "+BufferLine"},
     { "gh", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
     { "gl", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
@@ -20,6 +26,34 @@ return {
     { "<C-a>p", "<cmd>tabprevious<cr>", desc = "Previous Tab"},
     { "<C-a>c", "<cmd>tabnew<cr>", desc = "New Tab"},
     { "<C-a>x", "<cmd>tabclose<cr>", desc = "Close Tab"},
+
+    {
+      "<C-a>m",
+      function()
+        local tabIndex = vim.api.nvim_tabpage_get_number(0)
+        print('Tab Index: ' .. tabIndex)
+        if tabIndex == #vim.api.nvim_list_tabpages() then
+          vim.cmd('tabmove 0')
+        else
+          vim.cmd('tabmove +1')
+        end
+      end,
+      desc = "Move Tab Right"
+    },
+
+    { 
+      "<C-a>M",
+      function()
+        local tabIndex = vim.api.nvim_tabpage_get_number(0)
+        if tabIndex == 1 then
+          vim.cmd('tabmove $')
+        else
+          vim.cmd('tabmove -1')
+        end
+      end,
+      desc = "Move Tab Left"
+    },
+
     { "<C-a>r", 
       function() 
         local newName = vim.fn.input('New name for tab')
@@ -27,19 +61,6 @@ return {
       end,
       desc = "Rename Tab"
     },
-    -- { "<leader>T", "", desc = "+BufferLine Tabs"},
-    -- { "<leader>Tn", "<cmd>tabnext<cr>", desc = "Next Tab"},
-    -- { "<leader>Tp", "<cmd>tabprevious<cr>", desc = "Previous Tab"},
-    -- { "<leader>TN", "<cmd>tabnew<cr>", desc = "New Tab"},
-    -- {
-    --   "<leader>Tr", 
-    --   function() 
-    --     local newName = vim.fn.input('New name for tab')
-    --     vim.cmd('BufferLineTabRename ' .. newName)
-    --   end,
-    --   desc = "Rename Tab"
-    -- },
-
 
     },
     (function()
@@ -50,6 +71,7 @@ return {
       return numBindings
     end)()
   ),
+
   opts = {
     options = {
       -- stylua: ignore
@@ -62,6 +84,7 @@ return {
       right_mouse_command = function(n) require('snacks').bufdelete(n) end,
       diagnostics = "nvim_lsp",
       always_show_bufferline = true,
+
       custom_filter = function(bufnr, bufnrs)
         local bufType = vim.api.nvim_get_option_value('filetype', {buf=bufnr})
         local blacklist = {
@@ -73,11 +96,13 @@ return {
         }
         return not blacklist[bufType]
       end,
+
       diagnostics_indicator = function(_, _, diag)
         local ret = (diag.error and icons.diagnostics.Error .. diag.error .. " " or "")
           .. (diag.warning and icons.diagnostics.Warn .. diag.warning or "")
         return vim.trim(ret)
       end,
+
       offsets = {
         {
           filetype = "neo-tree",
@@ -91,6 +116,7 @@ return {
       },
     },
   },
+
   config = function(_, opts)
     require("bufferline").setup(opts)
     -- Fix bufferline when restoring a session
