4505a3a55b9d56fe2bac14f10eda79f8e2fb09c7
harry feldman <harry.feldman99@gmail.com>
2025-12-06 12:41:22 -0600

    Configured fzf-native for linux

--- lua/plugins/editor/fzfNativeTelescope.lua
+++ lua/plugins/editor/fzfNativeTelescope.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-telescope/telescope-fzf-native.nvim',
-  -- build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
-  build = {'cmake -S. -Bbuild "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" -DCMAKE_BUILD_TYPE=Release', 'cmake --build build --config Release'},
+  build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+  -- build = {'cmake -S. -Bbuild "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" -DCMAKE_BUILD_TYPE=Release', 'cmake --build build --config Release'},
   --
   -- config = function() 
   --   require('telescope').load_extension('fzf')
@@ -9,8 +9,11 @@ return {
   -- opts = {},
   dependencies = {
     'nvim-telescope/telescope.nvim'
-  }
-
+  },
+  config = function(plugin) 
+    local telescope = require('telescope')
+    telescope.load_extension('fzf')
+    telescope.load_extension('file_browser')
+    telescope.load_extension('undo')
+  end,
 }
-
-8888920012
