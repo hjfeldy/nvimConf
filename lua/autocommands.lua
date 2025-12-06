ba466d1687053224e25ec35d7fc3542b12920257
harry feldman <harry.feldman99@gmail.com>
2025-12-06 17:14:29 -0600

    Added autocmd logic to toggle the neoWin terminal window whenever a fugitive window is opened/closed

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -3,8 +3,9 @@ local util = require('util')
 local resession = require('resession')
 local telescopeUtils = require('helpers.telescope.utils')
 local telescopeConf = require('helpers.telescope.config')
+local terms = require('neoWin.terminals')
 
--- clear fugitive buffers when 
+-- clear fugitive buffers when their windows are deleted
 api.nvim_create_autocmd('BufReadPost', {
   pattern = { 'fugitive://*' },
   callback = function()
@@ -12,12 +13,13 @@ api.nvim_create_autocmd('BufReadPost', {
   end
 })
 
-api.nvim_create_autocmd('BufReadPost', {
-  pattern = { '*.md' },
-  callback = function()
-    vim.cmd('TSBufEnable highlight')
-  end
-})
+-- For Markview (is this necessary?)
+-- api.nvim_create_autocmd('BufReadPost', {
+--   pattern = { '*.md' },
+--   callback = function()
+--     vim.cmd('TSBufEnable highlight')
+--   end
+-- })
 
 api.nvim_create_autocmd('BufReadPost', {
   pattern = { '*' },
@@ -34,6 +36,8 @@ api.nvim_create_autocmd('BufReadPost', {
   end
 })
 
+-- Load Session for the current directory at startup
+-- (if no cmdline args were passed to nvim)
 local ENTERED_CWD = nil
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
@@ -46,16 +50,55 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
   nested = true,
 })
+
+-- Save Session for the current directory at shutdown
 vim.api.nvim_create_autocmd("VimLeavePre", {
   callback = function()
     resession.save(ENTERED_CWD or vim.fn.getcwd(), { notify = false })
   end,
 })
+
+-- Record that a cmdline arg was passed to Neovim
 vim.api.nvim_create_autocmd('StdinReadPre', {
   callback = function()
     -- Store this for later
     vim.g.using_stdin = true
   end,
+})
+
+--- Keep track of whether or not a fugitive window (git status, git log, etc) is open in each tab
+--- This allows us to close and reopen the terminal window whenever a fugitive window
+--- @type { [integer]: boolean }
+local FUGITIVE_STATUS = {}
+
+--- Reopen the terminal window when fugitive windows are closed 
+--- (only if the fugitive window triggered an auto-close of the terminal window when it was opened)
+vim.api.nvim_create_autocmd('WinClosed', {
+  pattern = {'*'},
+  callback = function(ev)
+    local tab = vim.api.nvim_get_current_tabpage()
+    local fugitiveStatus = FUGITIVE_STATUS[tab] and FUGITIVE_STATUS[tab]
+    local bufType = vim.bo[ev.buf].filetype
+    if bufType == 'fugitive' and fugitiveStatus then
+      terms.toggle()
+      FUGITIVE_STATUS[tab] = false
+    end
+
+  end
+})
+
+--- Close the terminal window (if its open) whenever a fugitive window is opened
+vim.api.nvim_create_autocmd('User', {
+  pattern = {'FugitiveEditor', 'FugitiveIndex', 'FugitivePager'},
+  callback = function(ev)
+    print('Caught fugitive event:\n' .. vim.inspect(ev))
+    local firstWindowId = terms.firstWindowId()
+    local tab = vim.api.nvim_get_current_tabpage()
+    if firstWindowId ~= nil then
+      terms.toggle()
+      FUGITIVE_STATUS[tab] = true
+    end
+  end
 })
 
 --- Whenever leaving a telescope prompt, turn off all dynamic lualine icons
--- lua/plugins/editor/fugitive.lua
+++ lua/plugins/editor/fugitive.lua
@@ -1,5 +1,6 @@
 return {
    "tpope/vim-fugitive",
+   lazy=false,
    config = function()
      local map = vim.api.nvim_set_keymap
      map('n', '<leader>Gl', '<cmd>G log --decorate<cr>', {desc='Git Log'})
