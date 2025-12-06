0129fc78dbe6856b58b5cce78e5371cc56606108
Harry Feldman <Harry.Feldman@ryan.com>
2025-12-05 22:41:27 -0600

    Tweaked fzf-native build command (windows)

--- lua/plugins/editor/fzfNativeTelescope.lua
+++ lua/plugins/editor/fzfNativeTelescope.lua
@@ -1,6 +1,8 @@
 return {
   'nvim-telescope/telescope-fzf-native.nvim',
-  build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+  -- build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+  build = {'cmake -S. -Bbuild "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" -DCMAKE_BUILD_TYPE=Release', 'cmake --build build --config Release'},
+  --
   -- config = function() 
   --   require('telescope').load_extension('fzf')
   -- end,
@@ -10,3 +12,5 @@ return {
   }
 
 }
+
+8888920012
