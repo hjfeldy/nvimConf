533ed51fcefc39f89ce917fb3351a4a8ada7bad6
harry feldman <harry.feldman99@gmail.com>
2026-07-20 12:58:56 -0500

    Use latest telescope version (rather than pinned 0.1.8)

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -1,7 +1,8 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    -- tag = '0.1.8',
+    version = '*',
     dependencies = {
       'nvim-lua/plenary.nvim',
       "nvim-tree/nvim-web-devicons",
