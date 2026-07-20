3cbb067c4b30ce6dc83d70ab67c6e410ef5520f4
harry feldman <harry.feldman99@gmail.com>
2026-07-20 09:41:22 -0500

    migrate treesitter to Neovim 0.12 API

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -1,22 +1,28 @@
--- enhanced syntax-highlighting / navigation
+local parsers = {
+  "c",
+  "python",
+  "javascript",
+  "typescript",
+  "java",
+  "c_sharp",
+  "lua",
+  "vim",
+  "vimdoc",
+  "query",
+  "markdown",
+  "markdown_inline",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = 'master',
+  branch = "main",
   lazy = false,
   build = ":TSUpdate",
-  -- dependencies = { "OXY2DEV/markview.nvim" },
-  ensure_installed = {
-    "c",
-    "python",
-    "javascript",
-    "typescript",
-    "java",
-    "csharp",
-    "lua",
-    "vim",
-    "vimdoc",
-    "query",
-    "markdown",
-    "markdown_inline"
-  },
+  config = function()
+    vim.treesitter.language.register("c_sharp", "cs")
+
+    -- The main-branch rewrite no longer consumes the old `ensure_installed`
+    -- option. `install()` is asynchronous and a no-op for installed parsers.
+    require("nvim-treesitter").install(parsers)
+  end,
 }
