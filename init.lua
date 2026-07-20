e4214166e20cd2fffdd33f6bfdb14c2a50abb687
harry feldman <harry.feldman99@gmail.com>
2026-07-20 12:59:55 -0500

    Added OSC52 (copy/paste over SSH)

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+-- Send the unnamed/system registers through the client terminal when editing
+-- remotely. This must be set before anything initializes the clipboard provider.
+if vim.env.SSH_CONNECTION or vim.env.SSH_TTY then
+  vim.g.clipboard = 'osc52'
+end
+
 require('config.lazy')
 require('config.lsp')
 require('highlights')
