e6045c2f2a0b4aee1bb9faed513d1e028828466b
harry feldman <harry.feldman99@gmail.com>
2026-08-01 13:49:44 -0500

    Added remote copy/paste support (OSC52)

--- init.lua
+++ init.lua
@@ -1,7 +1,18 @@
 -- Send the unnamed/system registers through the client terminal when editing
 -- remotely. This must be set before anything initializes the clipboard provider.
 if vim.env.SSH_CONNECTION or vim.env.SSH_TTY then
-  vim.g.clipboard = 'osc52'
+  local osc52 = require('vim.ui.clipboard.osc52')
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = osc52.copy('+'),
+      ['*'] = osc52.copy('*'),
+    },
+    paste = {
+      ['+'] = osc52.paste('+'),
+      ['*'] = osc52.paste('*'),
+    },
+  }
 end
 
 require('config.lazy')
