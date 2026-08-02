7549bf5577ed925386294da1024d2e5326f1a389
harry feldman <harry.feldman99@gmail.com>
2026-08-02 14:14:38 -0500

    Added quality-of-life keymaps for diffview

--- lua/plugins/editor/diffview.lua
+++ lua/plugins/editor/diffview.lua
@@ -6,21 +6,30 @@ return {
     {"<leader>GL", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview file history (all files)"},
     {"<leader>GD", "<cmd>DiffviewOpen<cr>", desc = "Diffview (all files)"},
   },
-  opts = {
-    hooks = {
-      view_opened = function(view) 
-        local tabNum = vim.api.nvim_tabpage_get_number(view.tabpage)
-        local prevTab = vim.api.nvim_list_tabpages()[tabNum-1]
-        local tabName = vim.api.nvim_tabpage_get_var(prevTab, 'name')
-        vim.api.nvim_tabpage_set_var(view.tabpage, 'name', tabName .. ' (diff-view)')
-        vim.cmd('tabmove -1')
-      end
-    },
-    keymaps = {
-      view = {
-        {'n', 'J', ']c', desc = 'Next Hunk' },
-        {'n', 'K', '[c', desc = 'Previous Hunk' },
+  opts = function() 
+    local actions = require("diffview.actions")
+    return {
+      hooks = {
+        view_opened = function(view) 
+          local tabNum = vim.api.nvim_tabpage_get_number(view.tabpage)
+          local prevTab = vim.api.nvim_list_tabpages()[tabNum-1]
+          local tabName = vim.api.nvim_tabpage_get_var(prevTab, 'name')
+          vim.api.nvim_tabpage_set_var(view.tabpage, 'name', tabName .. ' (diff-view)')
+          vim.cmd('tabmove -1')
+        end
+      },
+      keymaps = {
+        view = {
+          {'n', 'J', ']c', desc = 'Next Hunk' },
+          {'n', 'K', '[c', desc = 'Previous Hunk' },
+          {'n', '<leader>j', actions.select_next_entry, desc = 'Diff Next File' },
+          {'n', '<leader>k', actions.select_prev_entry, desc = 'Diff Previous File' },
+        },
+        file_panel = {
+          {'n', '<leader>j', actions.select_next_entry, desc = 'Diff Next File' },
+          {'n', '<leader>k', actions.select_prev_entry, desc = 'Diff Previous File' },
+        }
       }
     }
-  }
+  end
 }
