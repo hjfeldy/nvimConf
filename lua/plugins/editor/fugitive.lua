be5a8373a50c8d23e33ba59bae5d09fd5d6cccc2
harry feldman <harry.feldman99@gmail.com>
2026-08-02 13:50:43 -0500

    Added custom "Gdl" command to view a log of two revisions, starting at their merge base

--- lua/plugins/editor/fugitive.lua
+++ lua/plugins/editor/fugitive.lua
@@ -1,7 +1,40 @@
 return {
    "tpope/vim-fugitive",
-   cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite" },
-   keys = {
-     { '<leader>Gl', '<cmd>G log --decorate<cr>', desc = 'Git Log' },
-   },
+   cmd = { "Git", "G", "Gdl", "Gdiffsplit", "Gread", "Gwrite" },
+   -- keys = {
+   --   { '<leader>Gl', '<cmd>G log --decorate<cr>', desc = 'Git Log' },
+   -- },
+   config = function()
+     vim.api.nvim_create_user_command('Gdl', function(opts)
+       if #opts.fargs ~= 2 then
+         vim.notify('Usage: Gdl {ref-a} {ref-b}', vim.log.levels.ERROR)
+         return
+       end
+
+       local ref_a, ref_b = unpack(opts.fargs)
+       local result = vim.system(
+         { 'git', 'merge-base', ref_a, ref_b },
+         { cwd = vim.fn.getcwd(), text = true }
+       ):wait()
+
+       local merge_base = vim.trim(result.stdout or '')
+       if result.code ~= 0 or merge_base == '' then
+         local message = vim.trim(result.stderr or '')
+         vim.notify(message ~= '' and message or 'Unable to determine merge base', vim.log.levels.ERROR)
+         return
+       end
+
+       local args = {
+         'log',
+         '--decorate',
+         '--graph',
+         ref_a,
+         ref_b,
+         '--not',
+         merge_base .. '~1',
+       }
+       local command = table.concat(vim.tbl_map(vim.fn.shellescape, args), ' ')
+       vim.api.nvim_cmd({ cmd = 'G', args = { command } }, {})
+     end, { nargs = '+' })
+   end,
 }
